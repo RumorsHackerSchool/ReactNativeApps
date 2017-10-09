f21e8cca550d177ce25333b58fd4d4f00c9d1206
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-09 09:59:52 +0300

    update the singin at firebase in google

--- Auth/src/app.js
+++ Auth/src/app.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import {View, Text} from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
+
 class App extends Component{
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDb3qxKFhNZXikwFbwSCEDWm-ghrs0tKDc',
+      authDomain: 'authentication-35af0.firebaseapp.com',
+      databaseURL: 'https://authentication-35af0.firebaseio.com',
+      projectId: 'authentication-35af0',
+      storageBucket: 'authentication-35af0.appspot.com',
+      messagingSenderId: '519594912664'
+    });
+  }
   render(){
     return(
       <View>
