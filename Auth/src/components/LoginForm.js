9b59028426e5a12fa84b81d6b30e47ab766df5a3
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-09 14:25:12 +0300

    update the main page of the app, in this page the username and password entries are working

--- Auth/src/components/LoginForm.js
+++ Auth/src/components/LoginForm.js
@@ -3,19 +3,32 @@ import { Button, Card, CardSection, Input } from './common';
 
 
 class LoginForm extends Component {
-  state = { text: '' }
+  state = {
+     email: '',
+     password: ''
+   };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
-            value={this.state.text}
-            onChangeText={text => this.setState({ test })}
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry="password"
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
